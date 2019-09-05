50f8bdb36473cfa806d4d36063b2b578e8bd71e5
milunas <lmilunas@evojam.com>
2019-09-05 13:57:58 +0200

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+Build executable jar:
+gradle clean build
+
+Run locally:
+java -jar build/libs/gradle-spring-react-0.0.1-SNAPSHOT.jar
+
+Deploy on heroku:
+https://devcenter.heroku.com/articles/deploying-executable-jar-files
---
+++ src/main/java/com/milunas/gradlespringreact/GradleSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.milunas.gradlespringreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSpringReactApplication.class, args);
+	}
+
+}
